25dc52acdb7f4f82cd2719202fdc357f2e3583e0
bala-amypo <bala@amypo.in>
2025-12-21 18:58:36 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/StudentProfileServiceImpl.java
@@ -0,0 +1,22 @@
+package com.example.demo.service;
+
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.StudentProfile;
+import com.example.demo.repository.StudentProfileRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentProfileServiceImpl implements StudentProfileService {
+    private final StudentProfileRepository repository;
+
+    // Strict Constructor Injection
+    public StudentProfileServiceImpl(StudentProfileRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public StudentProfile getStudentById(Long id) {
+        return repository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("not found"));
+    }
+}
