34b124e87f9f1fb4eeb0261c9ef450a72eb1360f
bala-amypo <bala@amypo.in>
2025-12-23 08:55:40 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthResponse.java
+++ src/main/java/com/example/demo/dto/AuthResponse.java
@@ -1,11 +1,4 @@
 // src/main/java/com/example/demo/dto/AuthResponse.java
 package com.example.demo.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public record AuthResponse(String token, Long userId, String email, String role) {}
---
+++ src/main/java/com/example/demo/model/RoomType.java
@@ -0,0 +1,6 @@
+// src/main/java/com/example/demo/model/RoomType.java
+package com.example.demo.model;
+
+public enum RoomType {
+    SINGLE, DOUBLE, TRIPLE
+}
