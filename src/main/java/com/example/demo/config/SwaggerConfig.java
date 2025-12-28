1fd61916b7d1c5e4166fff1b06b04e6983997cf5
bala-amypo <bala@amypo.in>
2025-12-28 17:21:24 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -2,17 +2,27 @@ package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
-    // ⚠️ DO NOT rename this method
-    public OpenAPI api() {
+    @Bean
+    public OpenAPI customOpenAPI() {
+
+        Server httpsServer = new Server();
+        httpsServer.setUrl("https://9219.408procr.amypo.ai");
+        httpsServer.setDescription("HTTPS Server");
+
         return new OpenAPI()
                 .info(new Info()
                         .title("Hostel Roommate Compatibility Matcher API")
                         .version("1.0")
-                        .description("Backend APIs for Hostel Roommate Matching"));
+                        .description("Backend APIs for Hostel Roommate Compatibility System"))
+                .servers(List.of(httpsServer));
     }
 }
